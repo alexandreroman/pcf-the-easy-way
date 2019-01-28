f250ffc4bdbb1edd7c368406e1c30e01246923f9
Alexandre Roman <aroman@pivotal.io>
2019-01-28 21:41:45 +0100

    Bump MySQL to 2.5.3

--- scripts/import-mysql.sh
+++ scripts/import-mysql.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 PRODUCT_NAME="MySQL for PCF" \
-PRODUCT_VERSION="2.4.2" \
+PRODUCT_VERSION="2.5.3" \
 DOWNLOAD_REGEX="^MySQL for PCF" \
   ${HOME}/pcf/scripts/import-product.sh || exit 1
-IMPORTED_NAME="pivotal-mysql" IMPORTED_VERSION="2.4.2-build.9" ${HOME}/pcf/scripts/stage-product.sh || exit 1
-IMPORTED_NAME="pivotal-mysql" IMPORTED_VERSION="2.4.2-build.9" ${HOME}/pcf/scripts/configure-product.sh || exit 1
+IMPORTED_NAME="pivotal-mysql" IMPORTED_VERSION="2.5.3-build.7" ${HOME}/pcf/scripts/stage-product.sh || exit 1
+IMPORTED_NAME="pivotal-mysql" IMPORTED_VERSION="2.5.3-build.7" ${HOME}/pcf/scripts/configure-product.sh || exit 1
