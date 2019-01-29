c404cdf1640dd81d653fbbdf40f70a26d19c3b31
Alexandre Roman <aroman@pivotal.io>
2019-01-29 09:15:10 +0100

    Bump Redis to 2.0

--- scripts/import-redis.sh
+++ scripts/import-redis.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 PRODUCT_NAME="Redis for PCF" \
-PRODUCT_VERSION="1.14.4" \
+PRODUCT_VERSION="2.0.0" \
 DOWNLOAD_REGEX="Redis for PCF$" \
   ${HOME}/pcf/scripts/import-product.sh || exit 1
-IMPORTED_NAME="p-redis" IMPORTED_VERSION="1.14.4" ${HOME}/pcf/scripts/stage-product.sh || exit 1
-IMPORTED_NAME="p-redis" IMPORTED_VERSION="1.14.4" ${HOME}/pcf/scripts/configure-product.sh || exit 1
+IMPORTED_NAME="p-redis" IMPORTED_VERSION="2.0.0" ${HOME}/pcf/scripts/stage-product.sh || exit 1
+IMPORTED_NAME="p-redis" IMPORTED_VERSION="2.0.0" ${HOME}/pcf/scripts/configure-product.sh || exit 1
