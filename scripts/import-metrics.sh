67e0cf957f3cd40b465bcc4124b7c9b9b555639b
Alexandre Roman <aroman@pivotal.io>
2019-01-29 09:15:38 +0100

    Bump PCF Metrics to 1.6.0

--- scripts/import-metrics.sh
+++ scripts/import-metrics.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 PRODUCT_NAME="Pivotal Cloud Foundry Metrics" \
-PRODUCT_VERSION="1.5.2" \
+PRODUCT_VERSION="1.6.0" \
 DOWNLOAD_REGEX="PCF Metrics" \
   ${HOME}/pcf/scripts/import-product.sh || exit 1
-IMPORTED_NAME="apmPostgres" IMPORTED_VERSION="1.5.2-build.4" ${HOME}/pcf/scripts/stage-product.sh || exit 1
-IMPORTED_NAME="apmPostgres" IMPORTED_VERSION="1.5.2-build.4" ${HOME}/pcf/scripts/configure-product.sh || exit 1
+IMPORTED_NAME="apmPostgres" IMPORTED_VERSION="1.6.0-build.41" ${HOME}/pcf/scripts/stage-product.sh || exit 1
+IMPORTED_NAME="apmPostgres" IMPORTED_VERSION="1.6.0-build.41" ${HOME}/pcf/scripts/configure-product.sh || exit 1
