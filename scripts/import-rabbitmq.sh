692d60d09165e8ea44a30f89af54356e71d4b0fd
Alexandre Roman <aroman@pivotal.io>
2019-01-29 09:14:47 +0100

    Bump RabbitMQ to 1.15.3

--- scripts/import-rabbitmq.sh
+++ scripts/import-rabbitmq.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 PRODUCT_NAME="RabbitMQ for PCF" \
-PRODUCT_VERSION="1.13.12" \
+PRODUCT_VERSION="1.15.3" \
 DOWNLOAD_REGEX="RabbitMQ for PCF$" \
   ${HOME}/pcf/scripts/import-product.sh || exit 1
-IMPORTED_NAME="p-rabbitmq" IMPORTED_VERSION="1.13.12" ${HOME}/pcf/scripts/stage-product.sh || exit 1
-IMPORTED_NAME="p-rabbitmq" IMPORTED_VERSION="1.13.12" ${HOME}/pcf/scripts/configure-product.sh || exit 1
+IMPORTED_NAME="p-rabbitmq" IMPORTED_VERSION="1.15.3" ${HOME}/pcf/scripts/stage-product.sh || exit 1
+IMPORTED_NAME="p-rabbitmq" IMPORTED_VERSION="1.15.3" ${HOME}/pcf/scripts/configure-product.sh || exit 1
