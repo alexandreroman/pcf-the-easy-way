c731452aef8756a255545a104c734290c5cdf7a6
Alexandre Roman <aroman@pivotal.io>
2019-02-01 18:09:04 +0100

    Bump PAS to 2.4.2

--- bootstrap/gcp/init.sh
+++ bootstrap/gcp/init.sh
@@ -31,8 +31,8 @@ PCF_AZ_1="CHANGE_ME_AZ_1"                               # e.g. europe-west1-b
 PCF_AZ_2="CHANGE_ME_AZ_2"                               # e.g. europe-west1-c
 PCF_AZ_3="CHANGE_ME_AZ_3"                               # e.g. europe-west1-d
 
-OPSMAN_VERSION="2.4.1"
-PAS_VERSION="2.4.1"
+OPSMAN_VERSION="2.4.3"
+PAS_VERSION="2.4.2"
 PKS_VERSION="1.3.0"
 EOF
 
